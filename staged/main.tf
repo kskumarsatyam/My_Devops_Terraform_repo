19627c6d0c837fc90cdd86a553f79e97187b093a
ks jha <Jayajha528@gmail.com>
2026-04-29 16:31:28 +0000

    Updated main.tf

--- staged/main.tf
+++ staged/main.tf
@@ -20,4 +20,10 @@ resource "azurerm_storage_account" "example" {
   ]
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
  
