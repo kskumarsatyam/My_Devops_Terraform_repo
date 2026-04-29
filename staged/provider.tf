ae030b6783e70e23b82dac557be9de93c0109de2
ks jha <Jayajha528@gmail.com>
2026-04-29 16:01:56 +0000

    Updated provider.tf

--- staged/provider.tf
+++ staged/provider.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "4.70.0"
     }
   }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
 
   backend "azurerm" {}
 }
 
-provider "azurerm" {
-  features {}
-}
