1b9f58429e844176663b66a7ce34c1548aaaa5f5
ks jha <Jayajha528@gmail.com>
2026-04-29 04:11:49 +0000

    Updated main.tf

--- staged/main.tf
+++ staged/main.tf
@@ -7,3 +7,12 @@ resource "azurerm_resource_group" "rg" {
     managed_by  = "terraform"
   }
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname131214"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+ }
