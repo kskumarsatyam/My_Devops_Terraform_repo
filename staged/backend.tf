512fde81b5b7ecc6412723e7d079c8484b8e03f2
ks jha <Jayajha528@gmail.com>
2026-04-29 16:08:45 +0000

    Updated backend.tf

--- staged/backend.tf
+++ staged/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tf-state-rg"
-    storage_account_name = "tfstate12345"
+    resource_group_name  = "myResourceGroup"
+    storage_account_name = "mystorageacct15665542178"
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
