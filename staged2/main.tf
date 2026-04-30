49c8eeb6dbd9052646e8fab53240ec045df0f47d
ks jha <Jayajha528@gmail.com>
2026-04-30 19:35:25 +0000

    Updated main.tf

--- staged2/main.tf
+++ staged2/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
