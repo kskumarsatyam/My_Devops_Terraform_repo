e7fe11adffabf1a37cd8afd87497ae4c8ded21aa
ks jha <Jayajha528@gmail.com>
2026-04-30 18:41:56 +0000

    Updated backend.tf

--- staged2/backend.tf
+++ staged2/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
   backend "azurerm" {
     resource_group_name  = "myResourceGroup"
     storage_account_name = "mystorageacct15665542178"
@@ -13,4 +6,3 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
-
