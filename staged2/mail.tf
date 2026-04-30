de2d593bd3e7399d324971aee01d7dfd9db87b05
ks jha <Jayajha528@gmail.com>
2026-04-30 18:31:13 +0000

    Added mail.tf

---
+++ staged2/mail.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "st" {
+  # Sanitizes the name to be lowercase and alphanumeric only
+  name                     = lower(replace(var.st_name, "/[^a-zA-Z0-9]/", ""))
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
