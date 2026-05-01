12f1a4d72828d6990411fac0e637cf0dde44dcbc
ks jha <Jayajha528@gmail.com>
2026-05-01 07:15:16 +0000

    Updated main.tf

--- modular_approach/modules/storage/main.tf
+++ modular_approach/modules/storage/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "st" {
-  name                     = var.st_name
+  st_name                     = var.st_name
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
