b3fd694f374c91810c81f0a7152c7ae4596eab2b
ks jha <Jayajha528@gmail.com>
2026-05-01 06:31:09 +0000

    Updated main.tf

--- modular_approach/modules/storage/main.tf
+++ modular_approach/modules/storage/main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
 resource "azurerm_storage_account" "st" {
   name                     = var.st_name
   resource_group_name      = var.rg_name
