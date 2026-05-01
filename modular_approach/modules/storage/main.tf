fac5919ca93aa82694edd683f4f2d94786d8056e
ks jha <Jayajha528@gmail.com>
2026-05-01 08:14:13 +0000

    Updated main.tf

--- modular_approach/modules/storage/main.tf
+++ modular_approach/modules/storage/main.tf
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "st" {
   name                     = var.st_name
-  resource_group_name      = var.rg_name
-  location                 = var.location
+  resource_group_name      = resource_group_name = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location 
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
